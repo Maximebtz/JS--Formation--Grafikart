cb5f4762f5b701b383c1e28fdada2577e3f1030e
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-07-22 15:31:39 +0200

    Update

--- Function/app.js
+++ Function/app.js
@@ -1,16 +1,48 @@
+//  1
+
+    // const wrapper = document.querySelector('.wrapper')
+
+    // function canDrive(age, pays) {
+    //     if(
+    //         (age > 18 && pays === 'FR') ||
+    //         (age > 16 && pays === 'US')
+    //     ) {
+    //         wrapper.textContent = "Go vibin', but drive safe !!"
+    //     }else{
+    //         wrapper.textContent = "NOOOP it's not for now bro !!!"
+    //     }
+    // }
+
+    // console.log(canDrive(17, 'US'))
 
 
-const wrapper = document.querySelector('.wrapper')
+//  2 
 
-function canDrive(age, pays) {
-    if(
-        (age > 18 && pays === 'FR') ||
-        (age > 16 && pays === 'US')
-    ) {
-        wrapper.textContent = "Go vibin' !!"
-    }else{
-        wrapper.textContent = "NOOOp it's not for now bro !!!"
+    const wrapper = document.querySelector('.wrapper')
+
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * (max + 1)) 
     }
-}
 
-console.log(canDrive(17, 'US'))
+    const solution = getRandomInt(10)
+    console.log(solution)
+    
+    function isRight(n) {
+        return solution === n
+    }
+
+    function guess() {
+        const number = prompt('Entrez un chiffre')
+        return isRight(number)
+    }
+
+    for ( i = 0; i < 3; i++) {
+        if(guess()) {
+            console.log('Bien joué')
+            break;
+        } else if (i === 2) {
+            console.log('Nop')
+        }
+    }
+
